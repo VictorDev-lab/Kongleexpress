f45535e3657a4dc3cba8e034ea26b24e2b9869c4
VictorDev-lab <VicDev19@gmail.com>
2025-07-21 13:57:45 +0200

    Ready to deploy Kongle

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kongleexpress
+Kongleexpress.com is a website where you can order a real norwegian pinecone 
--- backend/script.js
+++ backend/script.js
@@ -31,5 +31,13 @@ document.addEventListener('DOMContentLoaded', () => {
         subForm.reset();
       });
     }
+  
+  fetch('/api/kongles', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(data),
+});
+
+  
   });
   
