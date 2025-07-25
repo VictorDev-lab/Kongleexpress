38a39f3124bbfc257bf1ab427f6e7758d123d832
VictorDev-lab <VicDev19@gmail.com>
2025-07-25 03:42:19 +0200

    if this aint working omg i swear

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,13 @@
 export default {
   root: ".",
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 }
